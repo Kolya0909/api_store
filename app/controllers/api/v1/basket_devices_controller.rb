ff4845619e88282b8d9ee63a3df951e24f9c34ac
Kolya Gal <kolyagal@MacBook-Air-Kolya.local>
2022-04-01 13:40:31 +0300

    complete_basket_func

--- app/controllers/api/v1/basket_devices_controller.rb
+++ app/controllers/api/v1/basket_devices_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::BasketDevicesController < ApplicationController
 
-  def index
-    render json: @current_user
+  def create
+    @basket = Basket.where(user_id: @current_user.first["user_id"]).take
+    @basket_device = BasketDevice.new(basket_id: @basket.id, device_id: params[:device_id])
+    if @basket_device.save
+      render json: @basket_device
+    end
   end
 
-  def create
-    @device = Device.find(params[:id])
-
+  def destroy
+    @basket_device = BasketDevice.find(params[:id])
+    if @basket_device.destroy
+      render json: {massage: "BasketDevice with id #{params[:id]}, was destroyed"}
+    end
   end
 
 end
--- app/controllers/api/v1/baskets_controller.rb
+++ app/controllers/api/v1/baskets_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::BasketsController < ApplicationController
 
-
+  def index
+    @basket = Basket.where(user_id: @current_user.first['user_id']).first
+    render json: BasketDevice.where(basket_id: @basket.id)
+  end
 
 end
